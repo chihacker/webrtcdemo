7fd502d4551253b5da72e06a7f8cf206267f2d90
daesoon.choi <daesoon.choi@navercorp.com>
2021-01-26 17:39:40 +0900

    first commit

---
+++ app/src/main/java/blog/cmcmcmcm/playground/webrtc/MainActivity.kt
@@ -0,0 +1,11 @@
+package blog.cmcmcmcm.playground.webrtc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
